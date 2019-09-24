b1042e39e1177f7942d54fe24fdcd701fb9f57f2
tianyang <tianyang6916@163.com>
2019-09-24 18:20:07 +0800

    Initial Commit

---
+++ Interview/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Interview
+//
+//  Created by 田洋 on 2019/9/24.
+//  Copyright © 2019 田洋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
